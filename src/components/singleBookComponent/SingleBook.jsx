4756122366e569858676cdfb7caa4dc1b4c78067
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-06-02 14:03:43 +0100

    MyBadge

--- src/components/singleBookComponent/SingleBook.jsx
+++ src/components/singleBookComponent/SingleBook.jsx
@@ -1,15 +1,16 @@
-import { Card, Col, Badge, Button } from "react-bootstrap";
+import { Card, Col, Badge, Button } from "react-bootstrap"
+import MyBadge from "../MyBadge"
 
-import "./singleBook.css";
+import "./singleBook.css"
 
 function selectBook(e) {
-  const element = e.currentTarget;
-  element.classList.toggle("selectedCard");
+  const element = e.currentTarget
+  element.classList.toggle("selectedCard")
 }
 
 function SingleBook(book) {
   // const [open, setOpen] = useState(false);
-  let genreColor = book.category === "fantasy" ? "blue" : "red";
+  let genreColor = book.category === "fantasy" ? "blue" : "red"
   //  book.category === "romance" ? '#fff' :  book.category === "horror" ? "yellow" :  book.category === "scifi" ? "green" :  book.category === "history" ? "orange" : "white"
   return (
     <Col lg={3} md={4} sm={6} className="p-3">
@@ -18,9 +19,7 @@ function SingleBook(book) {
         onClick={selectBook}
         className="h-100 w-100 text-center"
       >
-        <Badge style={{ color: { genreColor } }}>
-          {book.category.length === 0 ? "Uncatagorized" : book.category}
-        </Badge>
+        <MyBadge category={book.category} />
         <Card.Img variant="top" src={book.img} />
         <Card.Body style={{ color: { genreColor } }}>
           <h3>
@@ -35,7 +34,7 @@ function SingleBook(book) {
         </Card.Footer>
       </Card>
     </Col>
-  );
+  )
 }
-export { selectBook };
-export default SingleBook;
+export { selectBook }
+export default SingleBook
